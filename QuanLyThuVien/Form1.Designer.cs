9648d5cd5333b139265a8711f60fdbd40e60140b
Hoàng Văn Tân <CT030248@actvn.edu.vn>
2020-12-16 14:05:35 +0700

    QLTV main

---
+++ QuanLyThuVien/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace QuanLyThuVien
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lbLogin = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.tbPassWord = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Location = new System.Drawing.Point(158, 369);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(153, 68);
+            this.btnLogIn.TabIndex = 0;
+            this.btnLogIn.Text = "Đăng nhập";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            this.btnLogIn.Click += new System.EventHandler(this.logIn_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(509, 369);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(153, 68);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.logOut_Click);
+            // 
+            // lbLogin
+            // 
+            this.lbLogin.AutoSize = true;
+            this.lbLogin.Location = new System.Drawing.Point(195, 131);
+            this.lbLogin.Name = "lbLogin";
+            this.lbLogin.Size = new System.Drawing.Size(75, 17);
+            this.lbLogin.TabIndex = 2;
+            this.lbLogin.Text = "UserName";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(195, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(195, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PassWord";
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(370, 131);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(203, 22);
+            this.tbUserName.TabIndex = 5;
+            // 
+            // tbPassWord
+            // 
+            this.tbPassWord.Location = new System.Drawing.Point(370, 206);
+            this.tbPassWord.Name = "tbPassWord";
+            this.tbPassWord.Size = new System.Drawing.Size(203, 22);
+            this.tbPassWord.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 518);
+            this.Controls.Add(this.tbPassWord);
+            this.Controls.Add(this.tbUserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbLogin);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogIn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lbLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.TextBox tbPassWord;
+    }
+}
+
---
+++ QuanLyThuVien/ListDevice.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace QuanLyThuVien
+{
+    partial class ListDevice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.lbAll = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lb1);
+            this.panel1.Location = new System.Drawing.Point(29, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(741, 42);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lbAll);
+            this.panel2.Location = new System.Drawing.Point(29, 60);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(741, 378);
+            this.panel2.TabIndex = 1;
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb1.Location = new System.Drawing.Point(250, 14);
+            this.lb1.Margin = new System.Windows.Forms.Padding(3, 0, 3, 2);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(197, 26);
+            this.lb1.TabIndex = 0;
+            this.lb1.Text = "Danh sách thiết bị";
+            // 
+            // lbAll
+            // 
+            this.lbAll.FormattingEnabled = true;
+            this.lbAll.ItemHeight = 16;
+            this.lbAll.Location = new System.Drawing.Point(3, 3);
+            this.lbAll.Name = "lbAll";
+            this.lbAll.Size = new System.Drawing.Size(735, 372);
+            this.lbAll.TabIndex = 0;
+            // 
+            // ListDevice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "ListDevice";
+            this.Text = "ListDevice";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox lbAll;
+    }
+}
